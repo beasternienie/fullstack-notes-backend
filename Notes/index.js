6b6b8c686bb3493a9cdde6fd95780cc0fcb4e3cc
beasternienie <nieniedjones@gmail.com>
2026-05-11 23:10:54 -0500

    Removed cors middleware.

--- Notes/index.js
+++ Notes/index.js
@@ -3,9 +3,6 @@ const app = express()
 app.use(express.json())
 app.use(express.static('dist'))
 
-const cors = require("cors")
-app.use(cors())
-
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
